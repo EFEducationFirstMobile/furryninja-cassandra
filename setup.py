0bf108d0a66fadaf2356901364d42c5871981a1d
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2014-12-17 14:01:46 +0000

    chore: bumps furry ninja version

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
         'cassandra-driver',
         'cql',
         'blist',
-        'furryninja==0.1.16',
+        'furryninja==0.2.0',
         'pysandra-unit'
     ],
     dependency_links=[
-        'https://github.com/EFEducationFirstMobile/furry-ninja/tarball/feat/gevent#egg=furryninja-0.1.16'
+        'https://github.com/EFEducationFirstMobile/furry-ninja/tarball/master#egg=furryninja-0.2.0'
     ]
 )
