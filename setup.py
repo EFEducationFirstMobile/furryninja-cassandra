6f9da4378bcc2532adfa37a30b1524a01631e07c
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2015-06-02 18:04:32 +0100

    Update setup.py
    Before: retrieved code from master branch of furry-ninja
    Now: retrieves from tag

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'pysandra-unit==0.5',
     ],
     dependency_links=[
-        'git+ssh://git@github.com/EFEducationFirstMobile/furry-ninja.git#egg=furryninja-0.4.1',
+        'git+ssh://git@github.com/EFEducationFirstMobile/furry-ninja.git@v0.4.1#egg=furryninja-0.4.1',
         'git+https://github.com/Zemanta/pysandra-unit.git@fea_cassandra_unit_20#egg=pysandra-unit-0.5'
     ]
 )
